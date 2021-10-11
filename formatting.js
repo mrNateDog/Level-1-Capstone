d41b2ed2830500758ba1fb4852575a3ec7d741ad
mrNateDog <nathan.mcclure@gmail.com>
2021-10-10 20:17:15 -0500

    first push

---
+++ formatting.js
@@ -0,0 +1,12 @@
+function jumpA(comps) {
+  var top = document.getElementById(comps).offsetTop;
+  window.scrollTo(0, top);
+}
+function jumpB(exp) {
+  var top = document.getElementById(exp).offsetTop;
+  window.scrollTo(0, top);
+}
+function jumpC(edu) {
+  var top = document.getElementById(edu).offsetTop;
+  window.scrollTo(0, top);
+}
